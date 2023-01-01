e6de091ac53550eed8620e9b091dbdfb8e1460e3
Dee Jaiye <deejaiye@dees-mbp.mynet>
2023-01-01 14:44:12 +0000

    Add additional calculation logic to return ending score. Refactored repeat commands as functions

--- main.py
+++ main.py
@@ -9,19 +9,61 @@ card_deck = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
 player_cards = []
 player_score = 0
 
-for i in range(0,2):
-    new_card = random.choice(card_deck)
-    player_cards.append(new_card)
 
-for i in player_cards:
-    player_score += i
+def draw_card(num_draw: int):
+    for i in range(0, num_draw):
+        new_card = random.choice(card_deck)
+        player_cards.append(new_card)
+        global player_score
+        player_score += new_card
 
-print(f"Your cards are {player_cards}. Your current score is {player_score}")
+def show_pl_cards():
+    print(f"\nYour cards are {player_cards}. Your current score is {player_score}")
+
+draw_card(2)
+
+show_pl_cards()
 
 # PC initial draw
 pc_cards = []
 pc_score = 0
 new_card = random.choice(card_deck)
 pc_cards.append(new_card)
+pc_score += new_card
 
-print(f"Computer's first card: {pc_cards}")
+print(f"Computer's first card: {pc_cards}. Current Computer score is {pc_score}")
+
+# Calculate blackjack
+
+# Next turn?
+draw_again = input("\nWould you like to draw again? Y or N: ").lower()
+
+if draw_again == "n":
+    while pc_score < 16:
+        new_card = random.choice(card_deck)
+        pc_cards.append(new_card)
+        pc_score += new_card
+    print(f"\nComputer's ending cards: {pc_cards}. Computer score is {pc_score}")
+    print(f"Your score is {player_score}")
+
+    if player_score >21:
+        print("You went BUST! Computer wins!")
+    elif pc_score >21:
+        print("Computer went BUST! You win!")
+    elif player_score == pc_score:
+        print("It's a draw!")
+    elif player_score > pc_score:
+        print("You win!")
+    elif pc_score > player_score:
+        print("Computer wins!")
+
+
+
+
+if draw_again == "y":
+    draw_again = True
+    draw_card(1)
+    show_pl_cards()
+
+else:
+    pass
